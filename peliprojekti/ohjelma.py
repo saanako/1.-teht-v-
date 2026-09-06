da54b3de63444b59e544b6cfe6dbd5c97b366dab
Saana Kontio <saanakon@metropolia.fi>
2026-09-06 22:16:52 +0300

    Tein valmiiksi projektitehtävän 2.

--- peliprojekti/ohjelma.py
+++ peliprojekti/ohjelma.py
@@ -1,4 +1,17 @@
 nimi = input('kerro minulle nimesi: \n')
-ika = input('kerro minulle ikäsi: \n')
+ika = int(input('kerro minulle ikäsi: \n'))
 
-print(nimi, ika,)
+if (ika < 12):
+    print('olet alaikäinen!')
+    exit()
+if (ika > 80):
+    print('Onko vanhainkodisa tarpeeksi hyvä netti pelaamista varten?!')
+if (ika == 12):
+    print('onneksi olkoon nimi , olet tarpeeksi vanha peliä varten')
+else : 
+    print ('hei nimi !')
+
+
+
+
+
